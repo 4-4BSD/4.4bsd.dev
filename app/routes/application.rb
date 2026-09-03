030a6c4b5f26a432bd61a82a7bf15b1a9c00fefd
robert <robert@r.uby.dev>
2026-09-03 16:27:32 +0000

    routes: assign correct scope

--- app/routes/application.rb
+++ app/routes/application.rb
@@ -16,7 +16,7 @@ module Raven::Routes
       require_request_specific_tokens: false,
       check_header: true
     plugin :agent,
-            agents: [{class: Beastie, scope: :session}]
+            agents: [{class: Beastie, scope: Raven::Scopes::Session}]
 
     route do |r|
       r.public
